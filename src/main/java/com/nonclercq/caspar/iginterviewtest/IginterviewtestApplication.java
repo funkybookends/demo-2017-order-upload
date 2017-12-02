57ba128cf93bc649c19691b1751da93c7048a045
FunkyBookends <nonclercqc@gmail.com>
2017-12-02 12:17:45 +0000

    Commit spring initializr code.

---
+++ src/main/java/com/nonclercq/caspar/iginterviewtest/IginterviewtestApplication.java
@@ -0,0 +1,12 @@
+package com.nonclercq.caspar.iginterviewtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IginterviewtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IginterviewtestApplication.class, args);
+	}
+}
